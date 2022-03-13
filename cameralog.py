7163cb75388c12d6444cae7831a8668e0b0ada1a
Chris Diaz <Chris.diaz24@me.com>
2022-03-13 19:18:57 -0400

    Second Video - Templates

--- cameralog.py
+++ cameralog.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', title='Home')
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
