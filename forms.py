51332b5542f265ee6cf9bb37644640f31c40a021
Chris Diaz <u305543@gmail.com>
2022-03-23 14:51:59 -0400

    Forms and Requirements.txt

--- forms.py
+++ forms.py
@@ -1,8 +1,35 @@
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateTimeField, SelectField
 from wtforms.validators import DataRequired, Length
 
-class CreateUserForm(FlaskForm):
-    username = StringField('Username', validators=[DateRequired()])
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Create')
+    login = SubmitField('Login')
+
+class CreateUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    active = BooleanField('Active')
+    supervisor = BooleanField('Supervisor')
+    role = SelectField('Role', choices=[('sgt', 'Sergeant'), ('lt', 'Lieutenant'), ('des', 'DES')])
+
+class CreateTrainingDate(FlaskForm):
+    date = DateTimeField('Class Date and Time', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
+    comments = StringField('Comments', validators=[DataRequired()])
+
+class CreateOfficerForm(FlaskForm):
+    badge = StringField('Badge', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    rank = StringField('Rank', validators=[DataRequired()])
+
+class CreateLocationForm(FlaskForm):
+    location = StringField('Location', validators=[DataRequired()])
+
+class CreateReasonForm(FlaskForm):
+    reason = StringField('Reason', validators=[DataRequired()])
